e3d2f467b98b15b6c811b8696c1c37c8cbd74282
hechabao <zangyifan1@qq.com>
2020-10-14 15:06:37 +0800

    first commit

--- testCase/home_page/Register_test.py
+++ testCase/home_page/Register_test.py
@@ -18,6 +18,7 @@ class TestRegException(MyTestCase):
         :return:
         """
         mylogger = Logger(logger='正常登录').getlog()
+        print('111')
         rp = test_Register_Page(self.driver)
         rp.open()#打开
         mylogger.info("打开浏览器")
